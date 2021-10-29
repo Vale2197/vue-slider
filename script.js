aaeebcdef263f44baa88b643fc2c4d535c7a1e06
Vale2197 <valentinatrebbi97@gmail.com>
2021-10-29 14:19:07 +0200

    importato immagine, costruita struttura javascript base

---
+++ script.js
@@ -0,0 +1,38 @@
+
+const main = new Vue({
+    el: "#main",
+    data: {
+        images: [
+            {
+                immagineLink: "./consegna2/img/01.jpg",
+                titolo: "Svezia",
+                paragrafo: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,"
+
+            },
+            {
+                immagineLink: "./consegna2/img/02.jpg",
+                titolo: "Svizzera",
+                paragrafo: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,"
+
+            },
+            {
+                immagineLink: "./consegna2/img/03jpg",
+                titolo: "Gran Bretagna",
+                paragrafo: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,"
+
+            },
+            {
+                immagineLink: "./consegna2/img/04.jpg",
+                titolo: "Germania",
+                paragrafo: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,"
+
+            },
+            {
+                immagineLink: "./consegna2/img/05.jpg",
+                titolo: "Paradise",
+                paragrafo: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,"
+
+            },
+        ]
+    }
+})
